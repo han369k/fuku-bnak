99604b7c1cdf632807a51c4c1167e87d0133afb4
SuShih\SuShih <evan890909@gmail.com>
2026-04-28 11:19:11 +0800

    完成Entity相關

---
+++ src/main/java/com/javaeasybank/loan/entity/LoanContactLog.java
@@ -0,0 +1,28 @@
+package com.javaeasybank.loan.entity;
+
+import com.javaeasybank.loan.status.LoanContactChannel;
+import com.javaeasybank.loan.status.LoanContactStatus;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "LOAN_CONTACT_LOG")
+public class LoanContactLog {
+
+    @Id
+    private String logId;                  // log_id (PK)
+
+    private String applicationId;          // application_id (FK)
+
+    private String empId;                  // 聯繫人員
+
+    @Enumerated(EnumType.STRING)
+    private LoanContactStatus contactStatus;
+
+    @Enumerated(EnumType.STRING)
+    private LoanContactChannel contactChannel;
+
+    private LocalDateTime contactTime;
+    private String note;
+}
