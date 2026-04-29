20ab9027513ff31f97a3a72be29334c4178cd43b
SuShih\SuShih <evan890909@gmail.com>
2026-04-29 16:18:44 +0800

    fix

--- src/main/java/com/javaeasybank/loan/enums/LoanContactStatus.java
+++ src/main/java/com/javaeasybank/loan/enums/LoanContactStatus.java
@@ -2,7 +2,7 @@ package com.javaeasybank.loan.enums;
 
 public enum LoanContactStatus {
     NOT_CONTACTED,  // 未聯繫
-    ATTEMPED,       // 已嘗試未聯繫上
+    ATTEMPTED,       // 已嘗試未聯繫上
     REACHED,        // 已接通
     CONFIRMED,      // 已確認繼續
     DECLINED        // 客戶放棄
